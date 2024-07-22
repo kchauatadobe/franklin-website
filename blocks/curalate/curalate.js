d3bb7a19aa8ef7a3d78f7b405fa57518b98481e9
Kyle Chau <kchau@adobe.com>
2024-07-22 16:29:18 -0400

    curalate handle for non webp

--- blocks/curalate/curalate.js
+++ blocks/curalate/curalate.js
@@ -8,6 +8,8 @@ export default function decorate(block) {
         <picture>
             <source sizes="352.25px" 
                 srcset="${link}=/w/150?typ=webp 150w,${link}/w/300?typ=webp 300w,${link}/w/450?typ=webp 450w,${link}/w/600?typ=webp 600w" type="image/webp">
+                      <source sizes="352.25px" 
+                srcset="${link}=/w/150? 150w,${link}/w/300? 300w,${link}/w/450? 450w,${link}/w/600? 600w" type="image/webp">
             <img alt="..." 
                 src="https://edge.curalate.com/sites/anntaylor-wcpeme/experiences/custom-carousel-1597850128186/assets/imagePlaceholder.png">
         </picture>
